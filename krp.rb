6fb67dcbfddce586a7f5b492370cc7a8b9405901
koekeishiya <aasvi93@hotmail.com>
2017-03-10 11:30:39 +0100

    krp v1.0.0

--- krp.rb
+++ krp.rb
@@ -1,8 +1,8 @@
 class Krp < Formula
   desc "Utility to adjust keyrepeat settings for MacOS."
   homepage "https://github.com/koekeishiya/krp"
-  url "https://github.com/koekeishiya/krp/releases/download/v0.0.3/krp-0.0.3.zip"
-  sha256 "dd3cb556d27a9eca56607dc73e2ead82ff91ce6f36517f38ec76b479be03ebb0"
+  url "https://github.com/koekeishiya/krp/releases/download/v1.0.0/krp-1.0.0.zip"
+  sha256 "8fe6ead50a5a623f2e9667c85c08badfb0f4adfdd56b891163afe0ce270c09d0"
 
   def install
     bin.install "krp"
