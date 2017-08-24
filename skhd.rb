80bdd0d85880c888ac6f4c95a0c4ce10188d1852
Bigo <bigo@crisidev.org>
2017-08-24 10:38:54 +0100

    Improve skhd formula

--- skhd.rb
+++ skhd.rb
@@ -1,50 +1,86 @@
 class Skhd < Formula
   desc "Simple hotkey-daemon for macOS."
   homepage "https://github.com/koekeishiya/skhd"
-  url "https://github.com/koekeishiya/skhd/releases/download/v0.0.6/skhd-0.0.6.zip"
-  sha256 "d992545e0951974ba3da80c042649e96e49ae7d8a43609a8df6e73724277c5d7"
+  url "https://github.com/koekeishiya/skhd/archive/v0.0.6.zip"
+  sha256 "aaafcaa35b7343f6aacd37ee83ca8a042916f35bc2630924cc9e8bdb9706b9b9"
+  head "https://github.com/koekeishiya/skhd.git"
 
+  option "with-logging", "Redirect stdout and stderr to log files"
   def install
-    bin.install "skhd"
-    prefix.install %w[skhdrc]
-  end
-
-  plist_options :startup => false
-
-  def plist; <<-EOS.undent
-    <?xml version="1.0" encoding="UTF-8"?>
-    <!DOCTYPE plist PUBLIC "-//Apple//DTD PLIST 1.0//EN" "http://www.apple.com/DTDs/PropertyList-1.0.dtd">
-    <plist version="1.0">
-    <dict>
-      <key>Label</key>
-      <string>#{plist_name}</string>
-      <key>ProgramArguments</key>
-      <array>
-      <string>#{bin}/skhd</string>
-      </array>
-      <key>EnvironmentVariables</key>
-      <dict>
-          <key>PATH</key>
-          <string>#{bin}:/usr/local/bin:/usr/bin:/bin:/usr/sbin:/sbin</string>
-      </dict>
-      <key>KeepAlive</key>
-      <true/>
-      <key>StandardOutPath</key>
-      <string>/tmp/skhd.out</string>
-      <key>StandardErrorPath</key>
-      <string>/tmp/skhd.err</string>
-    </dict>
-    </plist>
-    EOS
+    (var/"log/skhd").mkpath
+    system "make", "install"
+    bin.install "#{buildpath}/bin/skhd"
+    (pkgshare/"examples").install "#{buildpath}/examples/skhdrc"
   end
 
   def caveats; <<-EOS.undent
-    Copy the example config from #{prefix}/skhdrc into your home directory.
-      cp #{prefix}/skhdrc ~/.skhdrc
+    Copy the example configuration into your home directory:
+        cp #{opt_pkgshare}/examples/khdrcrc ~/.khdrc
+
+    If the formula has been built with --with-logging, logs will be found in
+      #{var}/log/skhd/skhd.[out|err].log
     EOS
   end
 
+  plist_options :manual => "skhd"
+
+  if build.with? "logging"
+      def plist; <<-EOS.undent
+        <?xml version="1.0" encoding="UTF-8"?>
+        <!DOCTYPE plist PUBLIC "-//Apple//DTD PLIST 1.0//EN" "http://www.apple.com/DTDs/PropertyList-1.0.dtd">
+        <plist version="1.0">
+        <dict>
+          <key>Label</key>
+          <string>#{plist_name}</string>
+          <key>ProgramArguments</key>
+          <array>
+            <string>#{opt_bin}/skhd</string>
+          </array>
+          <key>EnvironmentVariables</key>
+          <dict>
+            <key>PATH</key>
+            <string>#{HOMEBREW_PREFIX}/bin:/usr/bin:/bin:/usr/sbin:/sbin</string>
+          </dict>
+          <key>RunAtLoad</key>
+          <true/>
+          <key>KeepAlive</key>
+          <true/>
+          <key>StandardOutPath</key>
+          <string>#{var}/log/skhd/skhd.out.log</string>
+          <key>StandardErrorPath</key>
+          <string>#{var}/log/skhd/skhd.err.log</string>
+        </dict>
+        </plist>
+        EOS
+      end
+  else
+      def plist; <<-EOS.undent
+        <?xml version="1.0" encoding="UTF-8"?>
+        <!DOCTYPE plist PUBLIC "-//Apple//DTD PLIST 1.0//EN" "http://www.apple.com/DTDs/PropertyList-1.0.dtd">
+        <plist version="1.0">
+        <dict>
+          <key>Label</key>
+          <string>#{plist_name}</string>
+          <key>ProgramArguments</key>
+          <array>
+            <string>#{opt_bin}/skhd</string>
+          </array>
+          <key>EnvironmentVariables</key>
+          <dict>
+            <key>PATH</key>
+            <string>#{HOMEBREW_PREFIX}/bin:/usr/bin:/bin:/usr/sbin:/sbin</string>
+          </dict>
+          <key>RunAtLoad</key>
+          <true/>
+          <key>KeepAlive</key>
+          <true/>
+        </dict>
+        </plist>
+        EOS
+      end
+  end
+
   test do
-    system "#{bin}/skhd", "--version"
+    assert_match "skhd #{version}", shell_output("#{bin}/skhd --version")
   end
 end
