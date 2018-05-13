af8ec567b31456d9f93129b249d3d996f0b62c01
koekeishiya <aasvi93@hotmail.com>
2018-05-13 12:22:26 +0200

    skhd v0.2.1

--- skhd.rb
+++ skhd.rb
@@ -1,8 +1,8 @@
 class Skhd < Formula
   desc "Simple hotkey-daemon for macOS."
   homepage "https://github.com/koekeishiya/skhd"
-  url "https://github.com/koekeishiya/skhd/archive/v0.2.0.zip"
-  sha256 "bd3e7a7f040e87fa4dbb44e26ce2b083c4cd4e5ec4d43c1e52caa5575a00324d"
+  url "https://github.com/koekeishiya/skhd/archive/v0.2.1.zip"
+  sha256 "62ea2fdd9e9e0849551425ff7292e5f139cc5338519f28f9efdde3f62b306b75"
   head "https://github.com/koekeishiya/skhd.git"
 
   option "with-logging", "Redirect stdout and stderr to log files"
