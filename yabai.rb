016363c8fb23b683278ad7248c51721f3dbbba2b
Dominik Lohmann <info@dominik-lohmann.de>
2019-09-12 17:55:52 +0200

    Add git-lfs dependency when building from source
    Fixes koekeishiya/yabai#248
    
    Sorry about this one, I knew I forgot something.

--- yabai.rb
+++ yabai.rb
@@ -6,6 +6,7 @@ class Yabai < Formula
   head "https://github.com/koekeishiya/yabai.git"
 
   depends_on :macos => :high_sierra
+  depends_on "git-lfs" => :build
 
   def install
     (var/"log/yabai").mkpath
