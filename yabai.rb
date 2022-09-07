ecec7b26855c58240aae5310ee733fd76fcaffd3
koekeishiya <aasvi93@hotmail.com>
2022-09-07 20:07:45 +0200

    yabai v4.0.3

--- yabai.rb
+++ yabai.rb
@@ -1,8 +1,8 @@
 class Yabai < Formula
   desc "A tiling window manager for macOS based on binary space partitioning."
   homepage "https://github.com/koekeishiya/yabai"
-  url "https://github.com/koekeishiya/yabai/releases/download/v4.0.2/yabai-v4.0.2.tar.gz"
-  sha256 "160da6f5a3e6fb49ab19eabdc181198962804cd9c66ed3bbffce495893886637"
+  url "https://github.com/koekeishiya/yabai/releases/download/v4.0.3/yabai-v4.0.3.tar.gz"
+  sha256 "ca752c144c113918d731436f1490bb075a43f273fb138ae10c7c2730832c3fdf"
   head "https://github.com/koekeishiya/yabai.git"
 
   depends_on :macos => :high_sierra
