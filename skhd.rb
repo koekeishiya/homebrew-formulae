65a6d9ccd6e3ab9b9f29c1ec7773606e966a0ec5
Jorge Pinilla Lopez <jorgepl@palantir.com>
2023-03-03 19:19:35 +1100

    skhd removed logging option

--- skhd.rb
+++ skhd.rb
@@ -5,8 +5,6 @@ class Skhd < Formula
   sha256 "64e40b4f65e9db1c4a4ce333b4978bbd84fb72df62e5d17dd2b6a41bf008ee10"
   head "https://github.com/koekeishiya/skhd.git"
 
-  option "with-logging", "Redirect stdout and stderr to log files"
-
   def install
     (var/"log/skhd").mkpath
     system "make", "install"
@@ -23,22 +21,14 @@ class Skhd < Formula
     EOS
   end
 
-  if build.with? "logging"
-    service do
-      run "#{opt_bin}/skhd"
-      environment_variables PATH: std_service_path_env
-      keep_alive true
-      log_path "#{var}/log/skhd/skhd.out.log"
-      error_log_path "#{var}/log/skhd/skhd.err.log"
-      process_type :interactive
-    end
-  else
-    service do
-      run "#{opt_bin}/skhd"
-      environment_variables PATH: std_service_path_env
-      keep_alive true
-      process_type :interactive
-    end
+
+  service do
+    run "#{opt_bin}/skhd"
+    environment_variables PATH: std_service_path_env
+    keep_alive true
+    log_path "#{var}/log/skhd/skhd.out.log"
+    error_log_path "#{var}/log/skhd/skhd.err.log"
+    process_type :interactive
   end
 
   test do
