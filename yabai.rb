11059686fb6993f12326ed55554b58f0de13c026
koekeishiya <aasvi93@hotmail.com>
2023-05-01 17:44:26 +0200

    yabai v5.0.4

--- yabai.rb
+++ yabai.rb
@@ -1,8 +1,8 @@
 class Yabai < Formula
   desc "A tiling window manager for macOS based on binary space partitioning."
   homepage "https://github.com/koekeishiya/yabai"
-  url "https://github.com/koekeishiya/yabai/releases/download/v5.0.3/yabai-v5.0.3.tar.gz"
-  sha256 "f3dfbb022e5f99803d18979d44a1d5ff72609bed620621dd95238c3009ca586a"
+  url "https://github.com/koekeishiya/yabai/releases/download/v5.0.4/yabai-v5.0.4.tar.gz"
+  sha256 "c7dcb6a179d4815d1ada3b66c30f5301d9dd2befd986d8ee9744b39a011994a2"
   head "https://github.com/koekeishiya/yabai.git"
 
   depends_on :macos => :big_sur
@@ -26,8 +26,11 @@ class Yabai < Formula
       cp #{opt_pkgshare}/examples/yabairc ~/.yabairc
       cp #{opt_pkgshare}/examples/skhdrc ~/.skhdrc
 
-    Logs will be found in
-      #{var}/log/yabai/yabai.[out|err].log
+    If you want yabai to be managed by launchd (start automatically upon login):
+      yabai --start-service
+
+    When running as a launchd service logs will be found in:
+      /tmp/yabai_<user>.[out|err].log
 
     If you are using the scripting-addition; remember to update your sudoers file:
       sudo visudo -f /private/etc/sudoers.d/yabai
@@ -36,43 +39,6 @@ class Yabai < Formula
       echo "$(whoami) ALL=(root) NOPASSWD: sha256:$(shasum -a 256 $(which yabai) | cut -d " " -f 1) $(which yabai) --load-sa"
 
     README: https://github.com/koekeishiya/yabai/wiki/Installing-yabai-(latest-release)#configure-scripting-addition
-    EOS
-  end
-
-  plist_options :manual => "yabai"
-
-  def plist; <<~EOS
-    <?xml version="1.0" encoding="UTF-8"?>
-    <!DOCTYPE plist PUBLIC "-//Apple//DTD PLIST 1.0//EN" "http://www.apple.com/DTDs/PropertyList-1.0.dtd">
-    <plist version="1.0">
-    <dict>
-      <key>Label</key>
-      <string>#{plist_name}</string>
-      <key>ProgramArguments</key>
-      <array>
-        <string>#{opt_bin}/yabai</string>
-      </array>
-      <key>EnvironmentVariables</key>
-      <dict>
-        <key>PATH</key>
-        <string>#{HOMEBREW_PREFIX}/bin:/usr/bin:/bin:/usr/sbin:/sbin</string>
-      </dict>
-      <key>RunAtLoad</key>
-      <true/>
-      <key>KeepAlive</key>
-      <true/>
-      <key>ThrottleInterval</key>
-      <integer>30</integer>
-      <key>StandardOutPath</key>
-      <string>#{var}/log/yabai/yabai.out.log</string>
-      <key>StandardErrorPath</key>
-      <string>#{var}/log/yabai/yabai.err.log</string>
-      <key>ProcessType</key>
-      <string>Interactive</string>
-      <key>Nice</key>
-      <integer>-20</integer>
-    </dict>
-    </plist>
     EOS
   end
 
